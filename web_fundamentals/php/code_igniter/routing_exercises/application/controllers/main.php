a9af3acfab58a34d1964f676408672bab3646c2c
unknown <nar3nce@gmail.com>
2021-04-14 09:40:10 +0800

    added Routing Exercises

---
+++ web_fundamentals/php/code_igniter/routing_exercises/application/controllers/main.php
@@ -0,0 +1,47 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Main extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		echo "I am Main Class!";
+	}
+
+    public function hello()
+	{
+		echo "Hello World!";
+	}
+
+    public function say($word)
+	{
+		echo "hi ".$word;
+	}
+
+    public function say_anything($word)
+	{
+		echo strtoupper($word);
+	}
+
+    public function danger()
+	{
+		redirect('/main');
+	}
+    
+    
+}
